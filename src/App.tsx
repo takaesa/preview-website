b93cfc29ccd0fb14a5a804bd4baae7ad08e9bffc
Takae <93442902+takaesa@users.noreply.github.com>
2025-05-10 13:01:40 +0700

    update css properties

--- src/App.tsx
+++ src/App.tsx
@@ -199,23 +199,48 @@ function App() {
 
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-        }}
-      >
-        {/* upload section area */}
+      <h1 id="header">Upload file, preview them easily</h1>
+      <h3 id="sub-header">
+        File and url upload made easy. Upload file/url to have a preview easily.
+      </h3>
+      <div className="upload-button-container">
         <input
           type="file"
           accept=".jpeg, .png, .jpg, .pdf, .docx, .xlsx"
-          multiple
           onChange={handleFileChange}
           hidden
           id="upload-button"
         />
-        <label htmlFor="upload-button">Choose File</label>
+        <label htmlFor="upload-button">
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            id="upload-file-icon"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                d="M17 17H17.01M15.6 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H8.4M12 15V4M12 4L15 7M12 4L9 7"
+                stroke="#ffbc3f"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+          <h3>Click here to upload and render your file</h3>
+          <p id="Mime-type">Supported files: .pdf .docx .xlsx .jpeg and .jpg</p>
+        </label>
+      </div>
+      <div className="separated-section-text">OR</div>
+      <div id="url-input-container">
         <input
           type="text"
           placeholder="Enter file url"
@@ -224,7 +249,7 @@ function App() {
           id="URL-input"
         />
         <button onClick={handleFetchContent} id="render-url-btn">
-          Render URL
+          Submit
         </button>
       </div>
       <div>
@@ -257,12 +282,20 @@ function App() {
           {fileType === "docx" && fileContent && (
             <div
               style={{
-                marginTop: "1rem",
-                padding: ".75rem",
-                border: "1px solid #ccc",
+                padding: "1rem",
+                backgroundColor: "transparent",
+                border: "1px solid white",
               }}
-              dangerouslySetInnerHTML={{ __html: fileContent }}
-            ></div>
+            >
+              <div
+                style={{
+                  padding: "1rem",
+                  backgroundColor: "white",
+                  border: "1px solid white",
+                }}
+                dangerouslySetInnerHTML={{ __html: fileContent }}
+              ></div>
+            </div>
           )}
 
           {fileType === "xlsx" && fileContent && (
